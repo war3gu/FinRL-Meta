ac412ce41599b26ba709ad08f31afa0e44accc79
ckive <danyang2023@u.northwestern.edu>
2022-01-07 08:37:39 -0600

    exposed models

---
+++ features/feature.py
@@ -0,0 +1,5 @@
+"""
+will read in from 1 file of format | time | tech_ind_1 | ... | tech_ind_2 | tic |
+and parse to user train_start - train_end, test_start - test_end and combine with
+ohclv data source for analysis
+"""
